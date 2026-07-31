93815f94069b52eb5e8a3d59371891dc7bbcfeac
WIZAPPLY <info@wizapply.com>
2026-07-31 16:16:34 +0900

    Pythonスクリプトの修正。

--- build_python/wizmoTest.py
+++ build_python/wizmoTest.py
@@ -11,13 +11,18 @@ except FileNotFoundError:
     print("WIZMO DLL NOT FOUND ERROR!")
     exit()
 
-wm.starter('',False)
+wm.starter('')
+wm.simple_motion_power_update(0.1,0.667)
+wm.simple_motion_ratio_update(0.0,1.0)
+wm.speed_gain_mode(wizmo.wizmoSpeedGain.Normal)
 
 while wm.is_running():
     #print(wm.get_status())
-    wm.simple_pose_update(0.0,0.0,0.0,0.0,0.0,0.0) #Write required to update [is_running]
+    wm.simple_pose_update(1.0,0.0,0.0,0.0,0.0,0.0
+    time.sleep(0.5)
+    wm.simple_pose_update(-1.0,0.0,0.0,0.0,0.0,0.0)
     wm.get_backlog(True)
-    time.sleep(0.1) #100ms
+    time.sleep(0.5) #500ms
 
 wm.close()
 wm.get_backlog(True)
